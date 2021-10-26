01936e25fd7f6b58b548b00ea2920162a77c9725
Emily Ekaireb <eekaireb@ucsd.edu>
2021-10-25 19:11:28 -0700

    ns3 linked

--- server/syncServer.py
+++ server/syncServer.py
@@ -5,6 +5,27 @@ import math
 from threading import Thread
 from server import Server
 from .record import Record, Profile
+from py_interface import *
+from ctypes import *
+
+# The environment (in this example, contain 'a' and 'b')
+# shared between ns-3 and python with the same shared memory
+# using the ns3-ai model.
+class Env(Structure):
+    _pack_ = 1
+    _fields_ = [
+        ('datarate', c_double),
+        ('latency', c_double),
+    ]
+
+# The result (in this example, contain 'c') calculated by python
+# and put back to ns-3 with the shared memory.
+class Act(Structure):
+    _pack_ = 1
+    _fields_ = [
+        ('c', c_double)
+    ]
+
 
 class Group(object):
     """Basic async group."""
@@ -66,7 +87,30 @@ class SyncServer(Server):
         rounds = self.config.fl.rounds
         target_accuracy = self.config.fl.target_accuracy
         reports_path = self.config.paths.reports
-
+        
+        mempool_key = 1234 # memory pool key, arbitrary integer large than 1000
+        mem_size = 4096                                             # memory pool size in bytes
+        memblock_key = 2338                                        # memory block key, need to keep the same in the ns-3 script
+        exp = Experiment(mempool_key, mem_size, 'simple_fed_learning', '../ns-allinone-3.34/ns-3.34')      # Set up the ns-3 environment
+        try:
+            exp.reset()                                             # Reset the environment
+            rl = Ns3AIRL(memblock_key, Env, Act)                    # Link the shared memory block with ns-3 script
+            pro = exp.run(show_output=True)    # Set and run the ns-3 script (sim.cc)
+            while not rl.isFinish():
+                with rl as data:
+                    if data == None:
+                        break
+                    # AI algorithms here and put tconda activate fl-py37he data back to the action
+                    print("a " + str(data.env.datarate))
+                    print("b " + str(data.env.latency))
+                    data.act.c = 0
+                
+            pro.wait()                                              # Wait the ns-3 to stop
+        except Exception as e:
+            print('Something wrong')
+            print(e)
+        finally:
+            del exp
         # Init self accuracy records
         self.records = Record()
 
